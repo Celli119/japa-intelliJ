ff74892b309f9914ff6501822ba2e380aedb3f56
celli119 <celli119@users.noreply.github.com>
2025-12-22 18:12:06 +0100

    build(ide): support IntelliJ 2025.3
    Update untilBuild from 252.* to 253.* for IntelliJ 2025.3 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("241")
-        untilBuild.set("252.*")
+        untilBuild.set("253.*")
     }
 
     // Disable searchable options building as plugin doesn't expose custom settings
